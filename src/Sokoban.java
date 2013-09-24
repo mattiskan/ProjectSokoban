738ebeb163158b08ea5cfc280ad1a16fd098c922
Mattis Kancans Envall <mattiskan@gmail.com>
2013-09-24 15:14:38 +0200

    Gjorde klart Sokoban.distance(Gamestate gs). Lade till egen pointklass med mamhattanDist

--- src/Sokoban.java
+++ src/Sokoban.java
@@ -1,13 +1,17 @@
+import java.util.*;
+
 public class Sokoban {
     public static final int FOUND = -1;
     public static final int NOT_FOUND = -2;
+
+    public Map map = null;
 
     // distances should be measured in ints, since
     // comparisons between floats are buggy.
 
     // TODO actually record path taken
     public String IDAStar(GameState initialState) {
-       int boundary = distance(initialState);
+	/*int boundary = distance(initialState);
        while(true) {
            int t = search(initialState, 0, boundary);
            if(t == FOUND) {
@@ -16,11 +20,12 @@ public class Sokoban {
                return null;
            }
            boundary = t;
-       }
+       }*/
+	return null;
     }
 
     public int search(GameState node, int g, int boundary, String path) {
-        if(isGoal(node)) {
+        /*if(isGoal(node)) {
             pathToGoal = path;
             return FOUND;
         }
@@ -37,14 +42,36 @@ public class Sokoban {
             }
             min = Math.min(t, min);
         }
-        return min;
+        return min;*/return 0;
     }
 
-    public int cost(GameState node1, GameState node2) {
-        return 0;
+
+    public int cost(GameState current, GameState goal) {
+        return 1;
     }
 
-    public int distance(GameState node) {
+
+    public int distance(GameState current) {
+	int distanceCost = 0;
+
+	Queue<Point> boxes = current.getBoxes();
+	HashSet<Point> goals = map.getOpenGoals();
+
+	while(!boxes.isEmpty()) {
+	    Point box = boxes.poll();
+	    int nearest = Integer.MAX_VALUE;
+	    Point nearestOpenGoal = null;
+	    for(Point goal : goals){
+		int distanceTo = box.manhattanDist(goal);
+		    if( distanceTo < nearest ){
+			nearest = distanceTo;
+			nearestOpenGoal = goal;
+		    }
+	    }
+
+	    distanceCost += nearest;
+	    goals.remove(nearestOpenGoal);
+	}
         return 0;
     }
 }
