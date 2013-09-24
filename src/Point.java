062c13d5d7c1620bdd9b5881e9eb6ff578fa1608
Axel Lewenhaupt <axellew@kth.se>
2013-09-24 20:29:32 +0200

    added hash code and equals

--- src/Point.java
+++ src/Point.java
@@ -11,4 +11,17 @@ public class Point {
 	return Math.abs(x - other.x) + Math.abs(y - other.y);
     }
     
+    @Override
+    public int hashCode() {
+	return x<<8 + y;
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+	if (other==null)
+	    return false;
+	Point o = (Point)other;
+	return o.x==x && o.y==y;
+    }
+    
 }
