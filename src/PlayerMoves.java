6094a1863f35b6b351e2ce469c482dd555e1a9db
Niclas Nilsson  <niclas@Barbaras-MacBook-Pro.local>
2013-09-26 15:25:57 +0200

    lol

--- src/PlayerMoves.java
+++ src/PlayerMoves.java
@@ -8,48 +8,48 @@ public class PlayerMoves {
     Queue<BFSRecord> q;
 
     public PlayerMoves(GameState gs){
-	this.gs = gs;
-	visited = new HashSet<Point>();
-	q = new LinkedList<BFSRecord>();
-	possiblePaths = new ArrayList<GameState>();
+        this.gs = gs;
+        visited = new HashSet<Point>();
+        q = new LinkedList<BFSRecord>();
+        possiblePaths = new ArrayList<GameState>();
     }
 
     public ArrayList<GameState> getPossibleStates(){
-	bfs();
-	return possiblePaths;
+        bfs();
+        return possiblePaths;
     }
 
     private void bfs(){
-	q.add(new BFSRecord(gs.player, gs.moveSeq));
-	visited.add(gs.player);
+        q.add(new BFSRecord(gs.player, gs.moveSeq));
+        visited.add(gs.player);
 
-	while(!q.isEmpty()){
-	    BFSRecord current = q.poll();
-	    
-	    for(int d=0; d<4; d++){
-		Point dir = GameState.move[d];
-		Point nextSquare = current.p.add(dir);
-		if(visited.contains(nextSquare))
-		   continue;
-		if(gs.hasBox(nextSquare)){
-		    Point to = nextSquare.add(dir);
-		    if(gs.getSquare(to).isOpen() && !GameState.map.isDeadSquare(to)){
-			possiblePaths.add(new GameState(gs, current.p, dir, new MoveSeq(current.m, gs.moveToChar[d])));
-		    }
-		} else if(gs.getSquare(nextSquare).isOpen()) {
-		    q.add(new BFSRecord(nextSquare, new MoveSeq(current.m, GameState.moveToChar[d])));
-		    visited.add(nextSquare);
-		}
-	    }
-	}
+        while(!q.isEmpty()){
+            BFSRecord current = q.poll();
+
+            for(int d=0; d<4; d++){
+                Point dir = GameState.move[d];
+                Point nextSquare = current.p.add(dir);
+                if(visited.contains(nextSquare))
+                    continue;
+                if(gs.hasBox(nextSquare)){
+                    Point to = nextSquare.add(dir);
+                    if(gs.getSquare(to).isOpen() && !GameState.map.isDeadSquare(to)){
+                        possiblePaths.add(new GameState(gs, current.p, dir, new MoveSeq(current.m, gs.moveToChar[d])));
+                    }
+                } else if(gs.getSquare(nextSquare).isOpen()) {
+                    q.add(new BFSRecord(nextSquare, new MoveSeq(current.m, GameState.moveToChar[d])));
+                    visited.add(nextSquare);
+                }
+            }
+        }
     }
 
     private class BFSRecord {
-	Point p;
-	MoveSeq m;
-	public BFSRecord(Point p, MoveSeq m){
-	    this.p = p;
-	    this.m = m;
-	}
+        Point p;
+        MoveSeq m;
+        public BFSRecord(Point p, MoveSeq m){
+            this.p = p;
+            this.m = m;
+        }
     }
 }
