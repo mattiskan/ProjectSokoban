f9b35f247bd8a1eb347e29191d5f8250b0cd412e
Axel Lewenhaupt <axel@x2d.org>
2013-09-26 16:45:28 +0200

    fixed deadlock code

--- src/Deadlock.java
+++ src/Deadlock.java
@@ -1,16 +1,17 @@
+
 import java.util.*;
 
 public class Deadlock {
 
     /*
-     * Checks whether there are any boxes that aren't on a goal and that
-     * can't move.
-     */
+* Checks whether there are any boxes that aren't on a goal and that
+* can't move.
+*/
     public static boolean isDeadlock(GameState gs) {
         List<Point> boxes = gs.getBoxes();
         for(Point box : boxes) {
             if(!isBoxOnGoal(gs, box)) {
-                if(isDeadlock(gs, box)) {
+                if(isDeadlock(gs, box, new HashSet<Point>())) {
                     return true;
                 }
             }
@@ -23,78 +24,30 @@ public class Deadlock {
     }
 
     /*
-     * Checks whether the box at point is deadlocked.
-     */
+* Checks whether the box at point is deadlocked.
+*/
     public static boolean isDeadlock(GameState gs, Point point, HashSet<Point> visited) {
-        if(canMove(gs, point)) {
+        if (gs.getSquare(point) == MapSquareType.WALL)
+            return true;
+        if (gs.getSquare(point).isOpen())
             return false;
-        }
+        if (visited.contains(point))
+            return true;
         visited.add(point);
-        for(Point p : neighbours(gs, point)) {
-            if(visited.contains(p)) {
-                continue;
-            }
-            if(!isDeadlock(gs, p, visited)) {
-                return false;
-            }
+        for (int i=0; i<4; i+=2) {
+            if (!isDeadlock(gs, point.add(GameState.move[i]), visited) && !isDeadlock(gs, point.add(GameState.reverseMove[i]), visited)) {
+        	return false;
+            } 
         }
         return true;
     }
 
-    public static boolean isDeadlock(GameState gs, Point point) {
-        return isDeadlock(gs, point, new HashSet<Point>());
-    }
-
-    public static List<Point> neighbours(GameState gs, Point point) {
-        ArrayList<Point> neighbours = new ArrayList<Point>();
-        if(isBox(gs, new Point(point.x-1, point.y))) {
-            neighbours.add(new Point(point.x-1, point.y));
-        }
-        if(isBox(gs, new Point(point.x+1, point.y))) {
-            neighbours.add(new Point(point.x+1, point.y));
-        }
-        if(isBox(gs, new Point(point.x, point.y-1))) {
-            neighbours.add(new Point(point.x, point.y-1));
-        }
-        if(isBox(gs, new Point(point.x, point.y+1))) {
-            neighbours.add(new Point(point.x, point.y+1));
-        }
-        return neighbours;
-    }
 
     /*
-     * Returns true if the point isn't a wall or box.
-     */
-    public static boolean walkable(GameState gs, Point point) {
-        if(gs.getSquare(point) == MapSquareType.BOX ||
-           gs.getSquare(point) == MapSquareType.BOX_ON_GOAL ||
-           gs.getSquare(point) == MapSquareType.WALL) {
-            return false;
-        }
-        return true;
-    }
-
-    /*
-     * Returns true if there is a box on point in gs.
-     */
+* Returns true if there is a box on point in gs.
+*/
     public static boolean isBox(GameState gs, Point point) {
-        return gs.getSquare(point) == MapSquareType.BOX || gs.getSquare(point) == MapSquareType.BOX_ON_GOAL;
+        return gs.getSquare(point).isBox();
     }
 
-    /*
-     * Returns true if there is a box on point in gs that can move
-     * unhindered in any direction, false otherwise. Doesn't consider
-     * the player's position.
-     */
-    public static boolean canMove(GameState gs, Point point) {
-        if(isBox(gs, point)) {
-            if(walkable(gs, new Point(point.x-1, point.y)) && walkable(gs, new Point(point.x+1, point.y))) {
-                return true;
-            }
-            if(walkable(gs, new Point(point.x, point.y-1)) && walkable(gs, new Point(point.x, point.y+1))) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
